ff008f8be570bd10a65b2ff7dcbd28077cfc2c0d
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-05-21 21:43:43 +0300

    feeds page finished

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -13,6 +13,7 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
+      console.log(session.user)
       const sessionUser = await User.findOne({
         email: session.user.email,
       });
--- promptopia/components/Feed.jsx
+++ promptopia/components/Feed.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+"use client"
+
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard'
+
+const PromptCardList = ({ data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((promp) => (
+        <PromptCard 
+          key={promp._id}
+          post={promp}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('')
+  const [prompts, setPrompts] = useState([])
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.value)
+  }
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      try {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+
+      setPrompts(data);
+      } catch(error) {
+        console.log("this is the error", error)
+      }
+    }
+
+    fetchPrompts();
+  }, [])
   return (
-    <div>Feed</div>
+    <div>
+      <section className='feed'>
+        <form className='relative w-full flex-center'>
+          <input 
+            type="text"
+            placeholder='Search for a tag or a username'
+            value={searchText}
+            onChange={handleSearchChange}
+            required 
+            className='search_input peer'
+          />
+        </form>
+
+        <PromptCardList 
+          data={prompts}
+          handleTagClick= {() => {}}
+        />
+      </section>
+    </div>
   )
 }
 
